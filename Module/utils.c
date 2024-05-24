cf49c2920bb0fbb24b1928d4fce7e1ff3131bdd9
Yukino <snowlychen2384@gmail.com>
2024-05-25 00:38:34 +0800

    Fix Segmentation fault at clear_fields()

--- Module/utils.c
+++ Module/utils.c
@@ -5,25 +5,34 @@ int set_fields(struct fields* fields, int p0, int o0, int p1, int o1, int p2, in
     /* selector */
     fields->selector.protocol = p0;
     fields->selector.offset = o0;
+    fields->selector.bytestream = NULL;
+    fields->selector.length = 0;
     /* field1 */
     fields->field1.protocol = p1;
     fields->field1.offset = o1;
+    fields->field1.bytestream = NULL;
+    fields->field1.length = 0;
     /* field2 */
     fields->field2.protocol = p2;
     fields->field2.offset = o2;
+    fields->field2.bytestream = NULL;
+    fields->field2.length = 0;
 }
 
 int clear_bytestream(struct fields* fields){
     /* selector */
-    free(fields->selector.bytestream);
+    if(fields->selector.bytestream != NULL)
+        free(fields->selector.bytestream);
     fields->selector.bytestream = NULL;
     fields->selector.length = 0;
     /* field1 */
-    free(fields->field1.bytestream);
+    if(fields->field1.bytestream != NULL)
+        free(fields->field1.bytestream);
     fields->field1.bytestream = NULL;
     fields->field1.length = 0;
     /* field2 */
-    free(fields->field2.bytestream);
+    if(fields->field2.bytestream != NULL)
+        free(fields->field2.bytestream);
     fields->field2.bytestream = NULL;
     fields->field2.length = 0;
 }
