35fd48244a0652e4428f0e94fcb3efe309a3ba9b
Yukino <snowlychen2384@gmail.com>
2024-05-25 00:05:55 +0800

    Redefine inject_file()

--- grammer.c
+++ grammer.c
@@ -2,7 +2,7 @@
 #include "Module/readwrite.h"
 #include "Module/steganography.h"
 
-int inject_file(char* infile, char* outfile, char* hiddenMsg, char* key,
+void inject_file(char* infile, char* outfile, char* hiddenMsg, char* key,
                 int proto0, int offset0, int proto1, int offset1, int proto2, int offset2){
 
     struct fields fields;
