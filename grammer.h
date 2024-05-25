c166b882d9d8c233714ddf4c79730ee8a5a8a771
Yukino <snowlychen2384@gmail.com>
2024-05-25 14:50:03 +0800

    Add help()

--- grammer.h
+++ grammer.h
@@ -1,4 +1,5 @@
 void inject_file(char* infile, char* outfile, char* hiddenfile, char* keyfile,
-                int proto0, int offset0, int proto1, int offset1, int proto2, int offset2);
+                 int proto0, int offset0, int proto1, int offset1, int proto2, int offset2);
 void select_file(char* infile, char* outfile, char* keyfile,
-                int proto0, int offset0, int proto1, int offset1, int proto2, int offset2);
+                 int proto0, int offset0, int proto1, int offset1, int proto2, int offset2);
+void help();
